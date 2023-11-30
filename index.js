240dd15447c6e0f2c742da4c3e394b69f7956fe1
rsmyers21 <rsmyers21@gmail.com>
2023-11-29 20:15:34 -0600

    Created binary search tree class

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 /* This program creates a Class that implements a binary search tree*/
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    isEmpty() {
+        return this.root === null;
+    }
+}
+
+tree = new BinarySearchTree()
+console.log(tree.isEmpty())
